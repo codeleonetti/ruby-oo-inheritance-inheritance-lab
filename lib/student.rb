96ac9c29d8ec972a369572c23afe8c27a4efc695
john <john.d.leonetti@gmail.com>
2021-03-10 14:41:04 -0800

    commit tree cd908d9a5928c11ea50a4203acf1c8688f784a3f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "../lib/user.rb"
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(student_knowledge)
+        @knowledge << student_knowledge
+
+    end
+
+    def knowledge
+        @knowledge
+
+    end
 
 end
